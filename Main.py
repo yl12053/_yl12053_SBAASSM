e8faf6b8063ebafbaae0f3650ed17af4df46afd4
yl12053 <qq18929261343@gmail.com>
2023-10-26 12:24:45 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,15 +3,21 @@ import UiInit
 import os, importlib
 from qfluentwidgets import qrouter
 
+#Wrapper for main procedure
+#To be plug into Background.run
 def wrapper():
+    #Store returnValue into list, to emulate pass by pointer in Python
     returnValue = [0]
     def main():
         from UI import GUI
         class BGUI(GUI):
+            #Custom exit function
             def exit(self, code=None):
                 if code is None:
                     code = self.app.exec_()
                 returnValue[0] = code
+
+            #Module initialization
             def refine(self):
                 for suspect in UiInit.members:
                     print("Loading %s"%suspect)
@@ -19,6 +25,7 @@ def wrapper():
                     importlib.import_module("UiInit.%s"%moduleName)
                     getattr(UiInit, moduleName).Init(self)
 
+            #GUI Initialization procedure & Main loop
             def launch(self):
                 qrouter.setDefaultRouteKey(self.ui.stackedWidget, self.ui.Homepage.objectName())
                 self.ui.stackedWidget.currentChanged.connect(self.interfaceChanged)
@@ -27,13 +34,15 @@ def wrapper():
                 super().launch()
 
         MainWindow = BGUI()
+        #Hijack exit function
         Background.exitFunc = MainWindow.forceQuit
         MainWindow.launch()
 
-
+    #Return value hook
     def hook():
         return returnValue[0]
 
     return main, hook
 
+#Start program with wrapper(SQL initialization)
 Background.run(*(wrapper()))
