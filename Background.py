b204e43ec140255b4e9294398d8573781964134c
yl12053 <qq18929261343@gmail.com>
2023-10-26 12:20:20 +0800

    Update Background.py for Comments

--- Background.py
+++ Background.py
@@ -5,14 +5,25 @@ from PyQt5.QtWidgets import QWidget
 
 from Modules import ModuleBase
 
+#Defining Global Variable for Cross-threading access of SQLite Database
+#Query queue
 que = queue.Queue()
+
+#Flag, to control the overall shutdown procedure
 running = False
+
+#Thread counting
 runProc = 0
+
+#Pre-defined list to store procedure to be run at start and end of program
+#Procedures will be appended(registered) during module registering
 procStartList = []
 procEndList = []
 
 exitFunc = sys.exit
 
+#Defining class Query.
+#Used for a single SQL Query
 class Query:
     def __init__(self, proc):
         self.proc = proc
@@ -23,50 +34,72 @@ class Query:
             pass
         return (self.ret, self.error)
 
+#Procedure to create a single query, which will put its' request into the queue and blocking for result
+#Thread-safe
+#Parameter injection method, safe from injection.
 def createQuery(sql, para = ()):
+    #Inner-procedure, to be passede into Query.proc
     def process(cursor):
         cursor.execute(sql, para)
         ret = cursor.fetchall()
         return ret
+    
+    #Create query object
     queryobj = Query(process)
+    #Put into queue
     que.put(queryobj)
     ret, err = queryobj.waitForAnswer()
+    #Return result
     if err:
         raise ret
     else:
         return ret
 
+#Thread for SQL
 def Thread():
     global runProc
+    #increase thread count
     runProc += 1
     database = sqlite3.connect("Data.db")
+    #While the program is not stopped yet
     while running:
+        #Get a single query from queue
         f = que.get()
+
+        #f will be None if and only if the program is stopped (or during stopping procedure)
         if f is None:
             continue
+
+        #create database cursor
         cursor = database.cursor()
+        
         try:
+            #pass the cursor into inter-procedure submitted from createQuery
             ret = f.proc(cursor)
+            #commit all possible update
             database.commit()
             f.ret = ret
             f.error = False
         except Exception as e:
             f.ret = e
             f.error = True
+    #Close the database once the program is stopped
     database.close()
+    #unregister the thread
     runProc -= 1
 
-
+#Register the thread
 procStartList.append(threading.Thread(target=Thread).start)
 procEndList.append(lambda: que.put(None))
 
-
+#Program init procedure
 def Start():
     global running
     running = True
     for func in procStartList:
         func()
 
+#Program shutdown procedure
 def Shutdown(hook = lambda: 0, blocking = False):
     global running
     print("Shutdown start")
@@ -74,6 +107,7 @@ def Shutdown(hook = lambda: 0, blocking = False):
     for func in procEndList:
         func()
     if blocking:
+        #block til all thread have peacefully shutdown
         WaitToDie()
     print("Done")
     exitFunc(hook())
@@ -83,6 +117,7 @@ def WaitToDie():
     while runProc:
         pass
 
+#decorator for func --> thread
 def threadWrapper(func):
     global runProc
     def wrap(*args, **kwargs):
@@ -101,6 +136,8 @@ def threadWrapper(func):
 
     return wrap
 
+#Unhandled-exception handler hijack
+#To shutdown the program peacefully before raise
 def genericEHook(func):
     def newExceptHook(*args, **kwargs):
         func(*args, **kwargs)
@@ -113,6 +150,7 @@ def genericEHook(func):
 sys.excepthook = genericEHook(sys.excepthook)
 threading.excepthook = genericEHook(threading.excepthook)
 
+#Program start procedure
 def run(func, rethook=lambda: 0):
     global running
     Start()
@@ -120,6 +158,7 @@ def run(func, rethook=lambda: 0):
     if running:
         Shutdown(hook = rethook)
 
+#Custom round function
 def mround(n, digits):
     p = round(n, digits)
     if p == int(p):
