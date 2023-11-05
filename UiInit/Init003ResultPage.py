e980e43f4f6682ccca534beecbe1eb4d0b835244
yl12053 <qq18929261343@gmail.com>
2023-11-06 01:36:46 +0800

    Update Init003ResultPage.py

--- UiInit/Init003ResultPage.py
+++ UiInit/Init003ResultPage.py
@@ -5,14 +5,16 @@ from FileReader import txt
 uiObj = None
 slots = []
 
-
+#init function
 def Init(obj):
     global uiObj
     uiObj = obj.ui
 
+    #Dropdown button for exporting
     menu = RoundMenu()
     exportToTxt = QAction("Plain Text File", parent=menu)
     exportToTxt.triggered.connect(lambda: txt.export(obj.ui.compoend))
     menu.addAction(exportToTxt)
     obj.ui.primaryDropDownPushButton.setMenu(menu)
+    #Back to home
     obj.ui.PushButton_2.clicked.connect(lambda: obj.ui.stackedWidget.setCurrentIndex(0))
